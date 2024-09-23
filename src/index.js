f3aa2be7369604e5ede9c16532f9e39d2c9b7458
vishal <vishrrajput02@gmail.com>
2024-09-23 16:38:05 +0530

    tested the mail sender and created the ticket model for email service

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 
 const { ServerConfig } = require('./config');
+
 const apiRoutes = require('./routes');
+const serverConfig = require('./config/server-config');
+const mailsender = require('./config/email-config');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+
+
+    const response = await mailsender.sendMail({
+        from:serverConfig.GMAIL_EMAIL,
+        to: "cmpn2----019@gmail.con", 
+        subject: "mail testing",
+        text: "kya haal bhai k", 
+       
+      });
+      console.log(response);
 });
